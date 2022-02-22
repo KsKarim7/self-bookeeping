af770425b8f8943dc17599aaf6821c613904e669
KsKarim <kskarim177@gmail.com>
2022-02-22 22:01:41 +0600

    getting closer

--- js/app.js
+++ js/app.js
@@ -1,83 +1,157 @@
-document.getElementById('calculate-button').addEventListener('click',function(){
+// // catching catchId through function 
+function catchId(id){
+    return document.getElementById(id)
+}
+
+function getValue(fieldId, isInput){
+
+    const inputField = document.getElementById(fieldId);
+    let valueInText = ' ';
+    if(isInput == true){
+        valueInText = inputField.value;  
+    }
+    else{
+        valueInText = inputField.innerText;
+    }
+    const value = parseFloat(valueInText);
+    return value;
+}
 
 
 
+catchId('calculate-button').addEventListener('click',function(){
+    // income field 
+    const totalIncome = getValue('total-income',true);    
     // food expenses 
-    const foodExpense = document.getElementById('food-expense');
-    const foodExpenseAmountText = foodExpense.value;
-    const newFoodExpenseAmount = parseFloat(foodExpenseAmountText);
-    console.log(newFoodExpenseAmount);
+    const foodExpense = getValue('food-expense',true);
 
     // rent expense
-    const rentExpense = document.getElementById('rent-expense');
-    const rentExpenseAmountText = rentExpense.value;
-    const newRentExpenseAmount = parseFloat(rentExpenseAmountText);
-    console.log(newRentExpenseAmount);
+    const rentExpense = getValue('rent-expense',true);
 
     // clothes expense
-    const clothExpense = document.getElementById('cloth-expense');
-    const clothExpenseAmountText = clothExpense.value;
-    const newClothExpenseAmount = parseFloat(clothExpenseAmountText);
-    console.log(newClothExpenseAmount);
+    const clothExpense = getValue('cloth-expense',true);    
 
-    // update total expense 
-    const totalExpense = document.getElementById('total-expense');
-    const previousTotalExpenseText = totalExpense.innerText;
-    const previousTotalExpense = parseFloat(previousTotalExpenseText);
-    const newTotalExpense = newFoodExpenseAmount + newRentExpenseAmount + newClothExpenseAmount;
-
-    totalExpense.innerText = newTotalExpense;
-
-    const totalIncome = document.getElementById('total-income');
-    const totalIncomeAmountText = totalIncome.value;
-    const presentTotalIncomeAmount = parseFloat(totalIncomeAmountText);
-    console.log(presentTotalIncomeAmount);
-
-    const remainingBalance = document.getElementById('remaining-balance');
-    const remainingBalanceAmountText = remainingBalance.innerText;
-    const previousRemainingBalanceAmount = parseFloat(remainingBalanceAmountText);
-    const newRemainingBalance = presentTotalIncomeAmount - newTotalExpense;
+    // update total expense     
+    const totalExpense = foodExpense + rentExpense + clothExpense;
+    document.getElementById('total-expense').innerText = totalExpense;
     
-    remainingBalance.innerText = newRemainingBalance;
+    if(totalExpense < totalIncome){
+        const balance = totalIncome - totalExpense;
+        catchId('balance').innerText = balance;
+    }
+    else{
+        const failError = catchId('expense-error');    
+        failError.classList.remove('d-none');
+        failError.classList.add('d-block');
+        // alert('expense is higher than your actual income')
 
-
+    }
+    
+    // console.log(totalExpense);
+    // console.log(balance);
+    
     // clear expenses inputs 
-    foodExpense.value = ' ';
-    rentExpense.value = ' ';
-    clothExpense.value = ' ';
-    totalIncome.value = ' ';
+    // foodExpense.value = ' ';
+    // rentExpense.value = ' ';
+    // clothExpense.value = ' ';
+    
 
     
    
 })
 
-document.getElementById('save-button').addEventListener('click',function(){
-    const savingAmount = document.getElementById('saving-amount');
-    const savingAmountText = savingAmount.value;
-    const savingAmountNumber = parseFloat(savingAmountText);
-    console.log(savingAmountNumber);
-
-    const savedAmount = document.getElementById('saved-amount');
-    const savedAmountText = savedAmount.innerText;
-    const savedAmountNumber = parseFloat(savedAmountText);
-    console.log(savedAmountNumber);
-
-    const totalIncome = document.getElementById('total-income');
-    const totalIncomeAmountText = totalIncome.value;
-    const presentTotalIncomeAmount = parseFloat(totalIncomeAmountText);
-    console.log(presentTotalIncomeAmount);
-
-    const finalSavedAmount =  presentTotalIncomeAmount * savingAmountNumber / 100;
+catchId('save-button').addEventListener('click',function(){
+    const savingAmount = getValue('saving-amount',true);
     
-    savedAmount.innerText = finalSavedAmount;
+    let savedAmount = getValue('saved-amount');
+    
+    const totalIncome2 = getValue('total-income',true);
+
+    const finalSavedAmount2 =  totalIncome2 * savingAmount / 100;
+    catchId('saved-amount').innerText = finalSavedAmount2; 
+
+    const remainingBalance = getValue('remaining-balance');
+    const previousBalance = getValue('balance');
+        let finalSavedAmount = getValue('saved-amount');
+    const finalRemainingBalance = previousBalance - finalSavedAmount;
+    // console.log(finalRemainingBalance);
+
+    if(finalSavedAmount < previousBalance){
+            
+        console.log(finalRemainingBalance);
+            
+        catchId('remaining-balance').innerText = finalRemainingBalance;
+    }
+    else{
+        const errorMessage = catchId('saving-fail');    
+        errorMessage.classList.remove('d-none');
+        errorMessage.classList.add('d-block');
+
+    }
+    
+    
+
 
     
-    
+
 })
 
-function catchID (id){
-    return document.getElementById(id)
+
+// comparing total savings with the remaining balance 
+// function remainder(){
+//     const remaining = parseFloat(catchId('balance').innerText);
+
+//     const savings = catchId('saved-amount');
+
+//     const balance = catchId("remaining-balance")
+//     console.log('balance');
+
+//     if(savings < balance){
+//         const balanceRemain =  balance - savings ;
+        
+
+//     }
+//     remaining.innerText = balanceRemain;
+// }
 
 
 
-}
+// function verifyInputs(){
+//     const failMessage = catchId('saving-fail');
+//     const failError = catchId('excess-expense');
+    
+//     const remainder =catchId('remaining-balance').innerText;
+//     const savings = catchId('saved-amount').innerText;
+
+//     if(savings > remainder){
+//         failMessage.style.display = 'none';
+//         failError.style.display = 'block'
+        
+//     }
+//     else{
+//         failMessage.style.display = 'block';
+//         failError.style.display = 'block'
+
+//     }
+
+// }
+// function verifyOtherInputs(){
+//     const failMessage = catchId('saving-fail');
+//     const income = catchId('total-income');
+//     const expenseMessage = catchId('total-expense').innerText;
+//     const failError = catchId('excess-expense');
+
+//     if(expenseMessage > income){
+//         failMessage.style.display = 'block';
+//         failError.style.display = 'none'
+        
+//     }
+//     else{
+//         failMessage.style.display = 'block';
+//         failError.style.display = 'block';
+
+//     }
+
+
+// }
+
